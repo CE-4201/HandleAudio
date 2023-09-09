15c09b46e65def35e21ef9ff4459ffdd93fffaa0
nyook941 <kennan.k.wu@gmail.com>
2023-09-08 22:09:56 -0500

    change bucket name

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import sendToS3
 
-bucketName = "base64-mp3-files-bucket"
+bucketName = "pre-transcribed-mp3-bucket"
 sendToS3Response = sendToS3.uploadToS3("./audioFiles/test.mp3", bucketName, "test.mp3")
