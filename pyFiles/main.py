4f471e982c3a2046ec5a7a3e312fe9007963df3d
Kennan Wu <kennan.k.wu@gmail.com>
2023-09-20 16:07:45 -0500

    while loop

--- pyFiles/main.py
+++ pyFiles/main.py
@@ -4,23 +4,25 @@ import time
 import editJson
 import handleMic
 
-fileName = handleMic.createFileName()
-handleMic.main_function(fileName)
+while True:
+    fileName = handleMic.createFileName()
+    print(fileName)
+    handleMic.main_function(fileName)
 
-bucketName = "pre-transcribed-mp3-bucket"
-sendToS3Response = handleS3.uploadToS3(f'./audioFiles/{fileName}', bucketName, fileName)
+    bucketName = "pre-transcribed-mp3-bucket"
+    sendToS3Response = handleS3.uploadToS3(f'./audioFiles/{fileName}', bucketName, fileName)
 
-getSendToTranscribeResponse = getSendToTranscribe.fetch(fileName)
-print(f"Send to Transcribe Lambda: Transcription Job {getSendToTranscribeResponse} sent")
+    getSendToTranscribeResponse = getSendToTranscribe.fetch(fileName)
+    print(f"Send to Transcribe Lambda: Transcription Job {getSendToTranscribeResponse} sent")
 
-audioLen = handleS3.findAudioLength(fileName)
-print("S3 Retrieval: Waiting", audioLen, "seconds")
-time.sleep(audioLen)
+    audioLen = handleS3.findAudioLength(fileName)
+    print("S3 Retrieval: Waiting", audioLen, "seconds")
+    time.sleep(audioLen)
 
-transcription = handleS3.fetch_transcription(getSendToTranscribeResponse)
-editJson.write_message_to_json("transcribed", transcription)
-print(f"Input: {transcription}")
+    transcription = handleS3.fetch_transcription(getSendToTranscribeResponse)
+    editJson.write_message_to_json("transcribed", transcription)
+    print(f"Input: {transcription}")
 
-chatGPT = getSendToTranscribe.fetchChatGPT(transcription)
-editJson.write_message_to_json("recieved", chatGPT)
-print(f"Output: {chatGPT}")
+    chatGPT = getSendToTranscribe.fetchChatGPT(transcription)
+    editJson.write_message_to_json("recieved", chatGPT)
+    print(f"Output: {chatGPT}")
